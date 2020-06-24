62ea31d9cd5309d076e31d3e30f00756eb937d7e
Sornavelvikas <32771185+Sornavelvikas@users.noreply.github.com>
2020-06-24 21:57:07 +0530

    Create scrap.php

---
+++ scrap.php
@@ -0,0 +1,18 @@
+<?php
+require_once "simple_html_dom.php";
+
+$dom = file_get_html("http://senthuherbals.blogspot.com/?m=1", false);
+$answer = array();
+$i =0;
+if(!empty($dom)) {
+foreach($dom->find("li.item") as $divClass) {
+//title
+print_r('checkli');
+foreach($divClass->find(".article-header .title.entry-title") as $title ) {
+    $answer[$i]['title'] = $title->plaintext;
+}
+$i++;
+}
+print_r($answer);
+print_r('check');
+die();
